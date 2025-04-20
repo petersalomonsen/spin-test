dcb01b37c21b4b21db41648dd7a4a9c6d09bafd5
Peter Salomonsen <pjsalomonsen@gmail.com>
2025-04-20 07:50:43 +0000

    wasm32-wasip1

---
+++ .devcontainer/install-dependencies.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cargo install cargo-component
+
+export WASI_SDK_VERSION=25.0
+ARCH=$(uname -m)
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+
+if [[ "$OS" == "darwin" && "$ARCH" == "arm64" ]]; then
+    WASI_SDK_RELEASE="wasi-sdk-${WASI_SDK_VERSION}-arm64-macos"
+elif [[ "$OS" == "darwin" && ( "$ARCH" == "x86_64" || "$ARCH" == "amd64" ) ]]; then
+    WASI_SDK_RELEASE="wasi-sdk-${WASI_SDK_VERSION}-x86_64-macos"
+elif [[ "$OS" == "linux" && "$ARCH" == "aarch64" ]]; then
+    WASI_SDK_RELEASE="wasi-sdk-${WASI_SDK_VERSION}-arm64-linux"
+elif [[ "$OS" == "linux" && ( "$ARCH" == "x86_64" || "$ARCH" == "amd64" ) ]]; then
+    WASI_SDK_RELEASE="wasi-sdk-${WASI_SDK_VERSION}-x86_64-linux"
+else
+    echo "Unsupported OS/architecture: $OS/$ARCH"
+    exit 1
+fi
+
+curl -LO https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-25/${WASI_SDK_RELEASE}.tar.gz
+tar xf ${WASI_SDK_RELEASE}.tar.gz
+echo "export WASI_SDK_PATH=`pwd`/${WASI_SDK_RELEASE}" >> $HOME/.bashrc
+echo export CC=\"\${WASI_SDK_PATH}/bin/clang --sysroot=\${WASI_SDK_PATH}/share/wasi-sysroot\" >> $HOME/.bashrc
+echo "export WIT_REQUIRE_F32_F64=0" >> $HOME/.bashrc
